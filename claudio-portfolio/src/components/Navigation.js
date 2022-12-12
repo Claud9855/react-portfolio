7bfb27071c7c1b3ed7789fd05d89dd21a5ea4968
Claud0505 <37052240+Claud9855@users.noreply.github.com>
2022-12-12 13:46:45 -0500

    added necessary components and implemented PortfolioContainer

--- claudio-portfolio/src/components/Navigation.js
+++ claudio-portfolio/src/components/Navigation.js
@@ -4,10 +4,18 @@ const Navigation = (props) => {
     return (
         <nav>
             <ul className={props.className}>
-                <li className="nav-item m-5 text-white text-uppercase">About Me</li>
-                <li className="nav-item m-5 text-white text-uppercase">Portfolio</li>
-                <li className="nav-item m-5 text-white text-uppercase">Contact</li>
-                <li className="nav-item m-5 text-white text-uppercase">Resume</li>
+                <li className="nav-item m-5 text-white text-uppercase">
+                    <a href="#AboutMe" onClick={() => props.handlePgaeChange('About')}>About Me</a>
+                </li>
+                <li className="nav-item m-5 text-white text-uppercase">
+                    <a href="#Portfolio" onClick={() => props.handlePgaeChange('Portfolio')}>Portfolio</a>
+                    </li>
+                <li className="nav-item m-5 text-white text-uppercase">
+                    <a href="#Contact" onClick={() => props.handlePgaeChange('Contact')}>Contact</a>
+                    </li>
+                <li className="nav-item m-5 text-white text-uppercase">
+                    <a href="#Resume" onClick={() => props.handlePgaeChange('Resume')}>Resume</a>
+                    </li>
             </ul>
         </nav>
     );
---
+++ claudio-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import Header from "./Header";
+import About from './About';
+import Resume from './Resume';
+import Contact from './Contact';
+import Portfolio from './Portfolio';
+
+const PortfolioContainer = () => {
+    const [currentPage, setCurrentPage] = useState('About');
+
+
+    const renderPage = () => {
+        if(currentPage === 'About') {
+            return <About/>;
+        }
+        else if(currentPage === 'Portfolio') {
+            return <Portfolio/>;
+        }
+        else if(currentPage === 'Contact') {
+            return <Contact/>;
+        }
+        else {
+            return <Resume/>;
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Header currentPage={currentPage} handlePageChange={handlePageChange}/>
+            {renderPage()}
+        </div>
+    );
+};
